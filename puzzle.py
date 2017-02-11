3d8bcda61701bb63b789055e0935349cc6ed3796
Julian Jaramillo <jjaram48@gmail.com>
2017-02-11 10:58:08 +0100

    experiment add number from range

--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,23 @@
-variable = 1
-
-initialState = []
-
+import random
 from random import randint
-print(randint(0,9))
 
-print(variable)
+w, h = 3, 3;  # Defines width and height of the initialState Array
+
+initialState = [[0 for x in range(w)] for y in range(h)]
+
+stateSpace = []  # Calculation of possibilities
+
+actions = []  # up,down, left, right
+
+# initGoalState = [[1, 2, 3][4, 5, 6][7, 8, 0]]
+
+# goalState = tuple(initGoalState)
+
+for i in initialState:
+    # randint(0, 9)
+    random.sample(range(0, 9), 3)
+    # for j in initialState:
+
+# print(randint(0,9))
+
+print(initialState)
